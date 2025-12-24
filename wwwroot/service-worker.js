a6abd2c8390e486ae63c3f15c9ef539c8f5663b6
kudryaWW <kirill35829@gmail.com>
2025-12-24 18:07:42 +0300

    Initial commit for Railway deploy

---
+++ wwwroot/service-worker.js
@@ -0,0 +1,23 @@
+﻿self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open("liya-timer-v1").then(cache => {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/css/styles.css",
+                "/js/timer.js",
+                "/images/sun.svg",
+                "/images/sunset.svg",
+                "/images/moon.svg"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
